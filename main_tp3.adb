b0740bea5168d2b9690701d2ceacba0bb16f1953
ACoutaux <103899450+ACoutaux@users.noreply.github.com>
2022-05-15 20:41:20 +0200

    Create tasks

--- main_tp3.adb
+++ main_tp3.adb
@@ -4,49 +4,61 @@ with Ada.Text_IO;
 use Ada.Text_IO;
 with Ada.Integer_Text_IO;
 use Ada.Integer_Text_IO;
+with ada.numerics.float_random; 
+use ada.numerics.float_random;
 
 procedure main_tp3 is 
 
-    x,y,z,read_length,cpt : INTEGER;   
+    read_length, cpt_loop : INTEGER;   
     My_File  : FILE_TYPE; 
+    my_buf : Shared_Items;
+    
+    task type Producer;
+    task type Consumer;
+
+    task body Producer is
+
+        G : generator;
+        f : FLOAT;
+        val : INTEGER;
+
+        begin
+        reset(G);
+        f := Random(G); -- test à 1
+        val := Integer(val);
+        my_buf.Set(val);
+        put("Producers puts"); 
+        put(val,2);
+        New_Line (1);
+    end Producer;
+
+    task body Consumer is 
+
+        val : INTEGER;
+
+        begin
+        my_buf.Get (val);
+        put("Consumer gets");
+        put(val,2);
+        New_Line (1);
+    end Consumer;
+
+    Producers : array(1..10) of producer;
+    Consumers : array(1..10) of consumer;
 
 begin
 
-    x:=0;
-    cpt := 1;
-
+    -- Lecture de la taille du buffer depuis le fichier de parametres
     Open (My_File, In_File, Name => "params.txt");
     Get (My_File, read_length);
     Put("Length read of buffer is ");
-    Put(read_length,3);
+    Put(read_length,2);
     New_Line (1);
     Close (My_File);
-
     Protected_Buffer.Length := read_length;
-
-    loop 
-        if cpt > Protected_Buffer.Length
-            then exit ;      
-            else 
-                Shared_Items.Set(x+cpt);
-                cpt := cpt+1;
-        end if ;
-    end loop ;
-
-    cpt := 1;
-
-    loop 
-        if cpt > Protected_Buffer.Length
-            then exit ;      
-            else 
-                Shared_Items.Get(z);
-                Put(z);
-                New_Line (1);
-                cpt := cpt+1;
-        end if ;
-    end loop ;
-
--- new _ pointeur sur tâche pour les tasks ADA
 
 
 end main_tp3;
+
+-- -------------  MODIFS -------------------------
+-- new _ pointeur sur tâche pour les tasks ADA
