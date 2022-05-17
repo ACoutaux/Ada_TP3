a67bf60fb1d7ebe1b21817b30742f0c73095540d
ACoutaux <103899450+ACoutaux@users.noreply.github.com>
2022-05-17 11:00:53 +0200

    Parameter Length private

--- protected_buffer.ads
+++ protected_buffer.ads
@@ -1,16 +1,21 @@
 package Protected_Buffer is
 
-   Length : Natural := 10; -- importer length du buffer en I/O
-   type Items is array (0 .. Length - 1) of Integer;
+
+   --Length : constraint := 5; -- importer length du buffer en I/O
+
+   type Items is array (Natural range <>) of Integer;
+   type Items_Access is access Items;
    
-   protected Shared_Items is
+   protected type Shared_Items (Length : Natural) is
       entry Get (I : out Integer); 
       entry Set (I : in Integer); 
       private
       First : Natural := 0;
-      Last : Natural := Length;
+      Last : Natural := Length - 1;
       Size : Natural := 0;
-      Buffer : Items;
+      Buffer : Items_Access := new Items (0 .. Length - 1);
    end Shared_Items;
+
+   type Shared_Items_Access is access Shared_Items;
 
 end Protected_Buffer;
