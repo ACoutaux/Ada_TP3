341452ec672f583158aa1b7ac8ad7b34510d450f
ACoutaux <103899450+ACoutaux@users.noreply.github.com>
2022-05-25 01:17:42 +0200

    Set remove poll mode cases

--- main_tp3.adb
+++ main_tp3.adb
@@ -12,21 +12,22 @@ use Ada.Calendar;
 procedure main_tp3 is 
 
     timed_delay : DURATION := 2.0; -- en secondes
-    current_time : TIME := Clock;
-    next_time : TIME := current_time + timed_delay;
+    current_time : Time := Clock;
+
+    period : Duration := 2.0;
 
     read_length : INTEGER;   
     My_File  : File_Type; 
     Items : Shared_Items_Access;
-    
-    Buffer_size : Natural;
 
+    mode : Sem_Type;
+    
     task type Producer is 
-       entry Initialize (P : Time_Span; D : Time; S : Sem_Type);
-<    end Producer;
+       entry Initialize (P : Duration; D : Time; S : Sem_Type);
+    end Producer;
 
     task type Consumer is
-       entry Initialize (P : Time_Span; D : Time; S : Sem_Type);
+       entry Initialize (P : Duration; D : Time; S : Sem_Type);
     end Consumer;
     
     task body Producer is
@@ -34,140 +35,79 @@ procedure main_tp3 is
         G : Generator;
         F : Float;
         Value : Integer;
-        Period : Time_Span;
+        Period : Duration;
         Deadline : Time;
         Semantic : Sem_Type;
         
     begin
-       Reset (G); -- Faire une seule fois
-       accept Initialize (P : Time_Span; D : Time; S : Sem_Type) do
-          Period := P;
-          Deadline := D;
-          Semantic := S;
-       end Initialize;
+        Reset (G); -- Faire une seule fois
+        accept Initialize (P : Duration; D : Time; S : Sem_Type) do
+            Period := P;
+            Deadline := D;
+            Semantic := S;
+        end Initialize;
        
-       loop
-          F := Random (G);
-          Value := Integer(10.0 * F);
-          Deadline := Deadline + Period;
-          case Semantic is
-             when Blocking =>
-                Items.Put (Value);
-             when Non_Blocking =>
-                Add (Items, Value);
-                
-             when Timed =>
-                select
-                   Items.Put(Value);
-                or
-                   delay until Deadline;
-                end select;
-          end case;
-       end loop;
+        loop
+            F := Random (G);
+            Value := Integer(10.0 * F);
+            Deadline := Deadline + Period;
+            case Semantic is
+                when Blocking =>
+                    Items.Put (Value);
+                    put("Producer puts ");
+                    put(Value);
+                    New_Line (2);
+                when Non_Blocking =>
+                    Add (Items, Value);    
+                    put("Producer puts ");
+                    put(Value);
+                    New_Line (2);
+                when Timed =>
+                    Offer (Items,Value, Deadline);
+                    put("Producer puts ");
+                    put(Value);
+                    New_Line (2);
+            end case;
+            delay(1.0);
+        end loop;
     end Producer;
 
-    --  task body Producer is
 
-    --      G : generator;
-    --      f : FLOAT;
-    --      val : INTEGER;
+    task body Consumer is
 
-    --      begin
-
-    --      select
-    --          accept non_blocked do
-    --              for i in 1 .. 10 loop
-    --                  reset(G);
-    --                  f := Random(G);
-    --                  val := Integer(f*10.0);
-    --                  Items.Set(val);
-    --                  put("Producers puts"); 
-    --                  put(val,3);
-    --                  New_Line (1);
-    --                  delay(1.0);   
-    --              end loop;
-    --          end non_blocked;
-    --      or
-    --          accept blocked do
-    --              for i in 1 .. 10 loop
-    --                  reset(G);
-    --                  f := Random(G);
-    --                  val := Integer(f*10.0);
-    --                  Items.Get(Buffer_size);
-    --                  while Buffer_size < Items.Length loop
-    --                      null;
-    --                  end loop;
-    --                  Items.Set(val);
-    --                  put("Producers puts"); 
-    --                  put(val,3);
-    --                  New_Line (1);
-    --                  delay(1.0);   
-    --              end loop;
-    --          end blocked;
-    --      or
-    --          accept timed do
-    --              for i in 1 .. 10 loop
-    --                  reset(G);
-    --                  f := Random(G);
-    --                  val := Integer(f*10.0);
-    --                  Items.Get_size (Buffer_size);
-    --                  delay until next_time; --Buffer_size < Items.Length or 
-    --                  Items.Set(val);
-    --                  put("Producers puts"); 
-    --                  put(val,3);
-    --                  New_Line (1);
-    --                  delay(1.0);   
-    --              end loop;
-    --          end timed;
-    --      end select;
+        Period : Duration;
+        Deadline : Time;
+        Semantic : Sem_Type;
+        Value : Integer;
         
-    --  end Producer;
-
-    task body Consumer is 
-
-        val : INTEGER;
-
-        begin
-
-        select
-
-            accept non_blocked do
-        
-                for i in 1 .. 10 loop
-                    Items.Get (val);
-                    put("Consumer gets");
-                    put(val,3);
-                    New_Line (1);
-                    delay(1.0);
-                end loop;
-            end non_blocked;
-        or 
-            accept blocked do
-                for i in 1 .. 10 loop
-                    Items.Get_size (Buffer_size);
-                    while Buffer_size > 0 loop
-                        Items.Get_size(Buffer_size);
-                    end loop;
-                    Items.Get (val);
-                    put("Consumer gets");
-                    put(val,3);
-                    New_Line (1);
-                    delay(1.0);
-                end loop;
-            end blocked;
-        or
-            accept timed do
-                for i in 1 .. 10 loop
-                    Items.Get_size (Buffer_size);
-                    delay until next_time; -- or Buffer_size > 0 
-                    Items.Get (val);
-                    put("Consumer gets");
-                    put(val,3);
-                    New_Line (1);
-                    delay(1.0);
-                end loop;
-            end timed;
-        end select;
+    begin
+        accept Initialize (P : Duration; D : Time; S : Sem_Type) do
+            Period := P;
+            Deadline := D;
+            Semantic := S;
+        end Initialize;
+       
+        loop
+            Deadline := Deadline + Period;
+            case Semantic is
+                when Blocking =>
+                    Items.Get(Value);
+                    put("Consumer gets ");
+                    put(Value);
+                    New_Line (2);                 
+                when Non_Blocking =>
+                    Value := Remove(Items); 
+                    put("Consumer gets ");
+                    put(Value);
+                    New_Line (2);
+                when Timed =>
+                    Value := Poll(Items, Deadline);
+                    put("Consumer gets ");
+                    put(Value);
+                    New_Line (2);
+            end case;
+            delay(1.0);
+        end loop;
     end Consumer;
 
     type Consumer_Access is access Consumer;
@@ -178,7 +118,8 @@ procedure main_tp3 is
 
     number_consumers : Natural;
     number_producters : Natural;
-    mode : INTEGER;
+    mode_index : INTEGER;
+    Wrong_Mode_Exception : exception;
 
 begin
 
@@ -190,21 +131,31 @@ begin
     New_Line (1);
     Get(My_File, number_producters); -- nombre de producteurs 2e ligne fichier params
     Get(My_File, number_consumers); -- nombre de consommateurs 3e ligne fichier params
-    Get(My_File, mode); -- mode non-blocked(0), blocked(1), timed(2) 4e ligne fichier params
+    Get(My_File, mode_index); -- mode non-blocked(0), blocked(1), timed(2) 4e ligne fichier params
     Close (My_File);
     Items := new Shared_Items (read_length - 1);
+
+    case mode_index is 
+        when 0 =>
+            mode := Non_Blocking;
+        when 1 =>
+            mode := Blocking;
+        when 2 =>
+            mode := Timed;
+        when others =>
+            raise Wrong_Mode_Exception;
+    end case;
     
     for I in 1 .. number_producters loop
-       P := new Producer;
-       P.Initialize ();
+        P := new Producer;
+        P.Initialize (period, current_time, mode);    
     end loop;
     for I in 1 .. number_consumers loop
-      C := new Consumer;
-      C.non_blocked;
+        C := new Consumer;
+        C.Initialize (period, current_time, mode);
     end loop;
 
 end main_tp3;
 
 -- -------------  MODIFS -------------------------
 -- Importation params ligne de commande
--- non-blocking / blocking / timed
