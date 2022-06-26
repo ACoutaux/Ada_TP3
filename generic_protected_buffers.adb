7d82ee2a14ebd0b9e31a4131c07150f0b49c0866
Laurent Pautet <laurent.pautet@telecom-paristech.fr>
2022-06-26 16:04:48 +0200

    proposal to enrich g_buffer

--- generic_protected_buffers.adb
+++ generic_protected_buffers.adb
@@ -1,26 +1,69 @@
 package body Generic_Protected_Buffers is
 
     protected body G_Buffer is 
-        entry Get (E : out Element; Done : out Boolean) 
+        entry Get (E : out Element) 
         when Size > 0 is
         begin
             E := Buffer (First);
             First := (First + 1) mod Length;
             Size := Size - 1;
-            Done := True;
         end Get;
-        entry Put (E : in Element; Done : out Boolean)
+        entry Put (E : in Element)
         when Size < Length is
         begin
             Last := (Last + 1) mod Length;
             Buffer (Last) := E;
             Size := Size + 1;
-            Done := True;
         end Put;
-        procedure Reinit_Done(D : Boolean) is
-        begin
-            Done := D;
-        end Reinit_Done;
     end G_Buffer;
-
+    
+    procedure Get (B : Buffer_Access; E : out Element) is
+    begin
+       B.Get (E);
+    end Get;
+    
+    procedure Remove (B : Buffer_Access; E: out Element) is
+    begin
+       select
+          B.Get (E);
+       else
+          raise Empty_Buffer_Exception;
+       end select;
+    end Remove;
+    
+    procedure Remove (B : Buffer_Access; E: out Element; Done : out Boolean) is
+    begin
+       select
+          B.Get (E);
+          Done := True;
+       else
+          Done := False;
+       end select;
+    end Remove;
+    
+    procedure Poll (B : Buffer_Access; T : Time; E: out Element) is
+    begin
+       select 
+          B.Get(E);
+       or 
+          delay until T;
+          raise Empty_Buffer_Exception;
+       end select;
+    end Poll;
+    
+    procedure Poll (B : Buffer_Access; 
+                    T : Time;
+                    E : out Element;
+                    Done : out Boolean)
+    is
+    begin
+       select 
+          B.Get(E);
+          Done := True;
+       or 
+          delay until T;
+          Done := False;
+       end select;
+    end Poll;
+       
 end Generic_Protected_Buffers;
--- generic_protected_buffers.ads
+++ generic_protected_buffers.ads
@@ -8,11 +8,9 @@ package Generic_Protected_Buffers is
    type Element_Table_Access is access Element_Table;
    
    protected type G_Buffer (Length : Natural) is
-      entry Get (E : out Element; Done : out Boolean); 
-      entry Put (E : in Element; Done : out Boolean); 
-      procedure Reinit_Done(D : Boolean);
-      private
-      Done : Boolean := False;
+      entry Get (E : out Element); 
+      entry Put (E : in Element); 
+   private
       First : Natural := 0;
       Last : Natural := Length - 1;
       Size : Natural := 0;
@@ -20,6 +18,12 @@ package Generic_Protected_Buffers is
       Buffer : Element_Table_Access := new Element_Table (0 .. Length - 1);
    end G_Buffer;
    type Buffer_Access is access G_Buffer;
+
+   procedure Get (B : Buffer_Access; E : out Element);
+   procedure Remove (B : Buffer_Access; E: out Element); -- raise exception
+   procedure Remove (B : Buffer_Access; E: out Element; Done : out Boolean);
+   procedure Poll (B : Buffer_Access; T : Time; E: out Element); -- raise exception
+   procedure Poll (B : Buffer_Access; T : Time; E: out Element; Done : out Boolean);
 
    Full_Buffer_Exception : exception;
    Empty_Buffer_Exception : exception;
